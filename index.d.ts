3f6a64e57f42c2d93f4fa3616815c782031ff687
Andrea Morandi <79252959+andreamorandi@users.noreply.github.com>
2024-03-07 09:19:49 +0100

    Fixed module name

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "react-native-navigation-bar-color" {
+declare module "@spicysparks/react-native-navigation-bar-color" {
     function changeNavigationBarColor(color: string, light?: boolean, animated?: boolean): void;
     function hideNavigationBar(): boolean;
     function showNavigationBar(): boolean;
